2940112d2509f32b8bbc41da0883cdfe399d57a6
soundarya-tech <soundaryagji@gmail.com>
2023-09-28 16:18:49 +0530

    grocery_list_App commit

---
+++ src/Content.js
@@ -0,0 +1,23 @@
+import ItemList from './ItemList'
+
+const Content = ({items,handleCheck,handleDelete}) => {
+
+return (
+    <>
+      {items.length ? (
+        
+        <ItemList
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+        />
+    
+      
+      ) : (<p style={{marginTop: '2rem'}}>Your List Is Empty</p>)}
+      
+     
+    </>
+  )
+}
+
+export default Content
